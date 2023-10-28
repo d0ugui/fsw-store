1338fd99a0947ed495a587c60a47cb8f3bc69a2b
Douglas Oliveira <douglaspo_97@outlook.com>
2023-10-28 14:54:11 -0300

    style: add footer desktop breakpoints

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,9 @@
 export const Footer = () => {
   return (
     <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-      © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      <div className="m-auto w-full max-w-[90rem] px-[6.25rem]">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </div>
     </div>
   );
 };
